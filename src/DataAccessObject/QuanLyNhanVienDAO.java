8de5519b0d8a2be508e61f6b7cc20a1e7d53ceb2
felixgaming100 <117741454+felixgaming100@users.noreply.github.com>
2025-05-23 02:46:53 +0700

    Add files via upload

---
+++ src/DataAccessObject/QuanLyNhanVienDAO.java
@@ -0,0 +1,73 @@
+package DataAccessObject;
+
+
+import controller.NhanVien;
+import config.ConnectDatabase;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class QuanLyNhanVienDAO {
+    
+    public List<NhanVien> getDanhSachNhanVien(){
+        List<NhanVien> danhsach = new ArrayList<>();
+        
+        try (Connection conn = ConnectDatabase.getConnection()){
+            String sql = "SELECT * FROM NhanVien";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                String ten = rs.getString("Ten");
+                String cccd = rs.getString("CCCD");
+                String sdt = rs.getString("SoDienThoai");
+                String taiKhoan = rs.getString("TaiKhoan");
+                String matKhau = rs.getString("MatKhau");
+                Date namSinh = rs.getDate("NamSinh");
+                String gioiTinh = rs.getString("GioiTinh");
+                String email = rs.getString("Email");
+
+                NhanVien nv = new NhanVien(ten, cccd, sdt, taiKhoan, matKhau, namSinh, gioiTinh, email);
+                danhsach.add(nv);
+            }
+        } catch (Exception e) {
+        }
+        return danhsach;
+    }  
+    public boolean themNhanVien(NhanVien nv){
+        try (Connection conn = ConnectDatabase.getConnection()){
+           String sql = "INSERT INTO NhanVien (Ten, CCCD, SoDienThoai, TaiKhoan, MatKhau, NamSinh, GioiTinh, Email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"; 
+           PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, nv.getTen());
+        stmt.setString(2, nv.getCccd());
+        stmt.setString(3, nv.getSdt());
+        stmt.setString(4, nv.getTaikhoan());
+        stmt.setString(5, nv.getMatkhau());
+        stmt.setDate(6, new java.sql.Date(nv.getNamsinh().getTime()));
+        stmt.setString(7, nv.getGioitinh());
+        stmt.setString(8, nv.getEmail());
+
+        return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean xoaNhanVien(String taiKhoan) {
+    try (Connection conn = ConnectDatabase.getConnection()) {
+        String sql = "DELETE FROM NhanVien WHERE TaiKhoan = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, taiKhoan);
+        return stmt.executeUpdate() > 0;
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+}
+
+    
+
